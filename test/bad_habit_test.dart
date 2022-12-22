fe1e1b470a8c6ca6b927f3146cc16dadfd03ec1f
Yazan Amer <yazan.amer@progressoft.com>
2022-12-22 09:47:13 +0300

    bad_habit_test.dart

--- test/bad_habit_test.dart
+++ test/bad_habit_test.dart
@@ -9,14 +9,21 @@ void main() {
     expect(habitTracker.getCurrentCount(), 0);
 
     habitTracker.doBadHabit();
-    var timeOfAction = DateTime.now();
-    timer.currentTime = timeOfAction;
+    DateTime timeOfFirstAction = configureMockTimer(timer);
     expect(habitTracker.getCurrentCount(), 1);
-    expect(habitTracker.getLastDone(), timeOfAction);
+    expect(habitTracker.getLastDone(), timeOfFirstAction);
 
     habitTracker.doBadHabit();
+    DateTime timeOfSecondAction = configureMockTimer(timer);
     expect(habitTracker.getCurrentCount(), 2);
+    expect(habitTracker.getLastDone(), timeOfSecondAction);
   });
+}
+
+DateTime configureMockTimer(MockTimer timer) {
+  var timeOfAction = DateTime.now();
+  timer.currentTime = timeOfAction;
+  return timeOfAction;
 }
 
 class MockTimer extends Timer {
